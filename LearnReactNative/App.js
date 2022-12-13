9e2831670be23cfc63bcc5b5a4e33f7c9c7fed2b
lbs1123 <lbs7230@naver.com>
2022-12-13 15:25:35 +0900

    '

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -101,10 +101,11 @@ const App = () => {
     //   </View>
     // </SafeAreaView>
 
-    <View style={{height: 400, backgroundColor: 'red'}}>
+    <View style={{height: '100%'}}>
       <WebView
-        source={{uri: 'https://infinite.red'}}
-        // source={{uri: 'https://www.naver.com'}}
+        // source={{uri: 'https://infinite.red'}}
+        // source={{uri: 'https://www.google.com'}}
+        source={{uri: 'https://www.google.com'}}
         style={{width: '100%', height: '100%'}}
       />
     </View>
@@ -127,6 +128,9 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  viewStyle: {
+    height: '100%',
   },
 });
 
